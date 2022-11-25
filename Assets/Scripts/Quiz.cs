6f2b9ddc858ea244699e46c8b7e9d60b266afd2d
mgarrod <mgarrod193@gmail.com>
2022-11-25 13:48:35 +0700

    added Game Manager, some bugs still need fixing

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -18,7 +18,7 @@ public class Quiz : MonoBehaviour
     Image buttonImage;
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
-    bool hasAnsweredEarly;
+    bool hasAnsweredEarly = true;
 
     [Header("Timer")]
     [SerializeField] Image timerImage;
@@ -32,7 +32,8 @@ public class Quiz : MonoBehaviour
     [SerializeField] Slider progressBar;
     public bool isComplete;
 
-    void Start()
+
+    void Awake()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
@@ -45,6 +46,11 @@ public class Quiz : MonoBehaviour
         timerImage.fillAmount = timer.fillFraction;
         if (timer.loadNextQuestion)
         {
+            if (progressBar.value == progressBar.maxValue)
+            {
+                isComplete = true;
+                return;
+            }
             hasAnsweredEarly = false;
             GetNextQuestion();
             timer.loadNextQuestion = false;
@@ -64,10 +70,6 @@ public class Quiz : MonoBehaviour
         timer.CancelTimer();
         scoreText.text = "score: " + scoreKeeper.CalculateScore() + "%";
 
-        if (progressBar.value == progressBar.maxValue)
-        {
-            isComplete = true;
-        }
     }
 
     void DisplayAnswer(int index)
